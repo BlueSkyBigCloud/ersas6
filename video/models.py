bbbe892908ba4c6b5de18e03fc8b134a59ca8c56
BlueSkyBigCloud <russeyl91@gmail.com>
2026-07-18 14:56:42 -0500

    PROFOROPS text edit 8

--- video/models.py
+++ video/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
 from embed_video.fields import EmbedVideoField
+from app.models import Company
 
 class Video(models.Model):
     name = models.CharField(max_length=100, unique=True)
     video = EmbedVideoField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='videos')
     def __str__(self):
         return self.name
     
--- video/views.py
+++ video/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from app.decorators import onboarded
 from .models import Video
-
+from app.models import Company
 
 @login_required
 @onboarded()
 def video_view(request):
-    videos = Video.objects.all()
+    user_company = getattr(request.user, 'company', None) 
+    videos = Video.objects.filter(company=user_company) if user_company else Video.objects.none()
+
     return render(request, 'video_view.html', {'videos': videos})
