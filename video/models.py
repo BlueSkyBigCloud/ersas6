6e8ce908606c37682f069d06fb1c220bb5d7d74f
BlueSkyBigCloud <russeyl91@gmail.com>
2026-07-18 15:06:42 -0500

    PROFOROPS text edit 9

--- video/models.py
+++ video/models.py
@@ -6,7 +6,7 @@ from app.models import Company
 class Video(models.Model):
     name = models.CharField(max_length=100, unique=True)
     video = EmbedVideoField()
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='videos')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True, related_name='videos')
     def __str__(self):
         return self.name
     
