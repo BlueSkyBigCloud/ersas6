42b63d090ec3f9bda8396a36acfa02410d9ca509
BlueSkyBigCloud <russeyl91@gmail.com>
2026-07-18 14:48:35 -0500

    PROFOROPS text edit 7

--- users/admin.py
+++ users/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('id', 'email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),  # Add phone_number here
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'is_onboarded', 'company', 'stripe_coupon_id', 'coupon_code', 'promotion_code')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_account_admin', 'is_superuser', 'groups', 'user_permissions', 'is_onboarded', 'company', 'stripe_coupon_id', 'coupon_code', 'promotion_code')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
@@ -20,7 +20,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2', 'phone_number'),  # Add phone_number here
         }),
     )
-    list_display = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'is_onboarded','is_staff', 'promotion_code', 'stripe_coupon_id', 'coupon_code')  # Add phone_number here
+    list_display = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'is_onboarded','is_staff', 'is_account_admin', 'promotion_code', 'stripe_coupon_id', 'coupon_code')  # Add phone_number here
     search_fields = ('id', 'email', 'first_name', 'last_name', 'phone_number')  # Add phone_number here
     ordering = ('id', 'email',)
     readonly_fields = ['id', 'email', 'first_name', 'last_name', 'phone_number']
