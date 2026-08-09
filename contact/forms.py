bb97867fc57d80fe5ba63ab2e96c3d838d88c03a
BlueSkyBigCloud <russeyl91@gmail.com>
2026-08-08 19:51:42 -0500

    PROFOROPS contact text form 2

--- contact/forms.py
+++ contact/forms.py
@@ -4,11 +4,14 @@ from .models import ContactUsRequest
 class ContactUsRequestForm(forms.ModelForm):
     class Meta:
         model = ContactUsRequest
-        fields = ['name', 'phone_number', 'email', 'address']
+        fields = ['name', 'last_name', 'company_name', 'phone_number', 'email', 'address']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Mark required fields
         self.fields['name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['company_name'].required = True
         self.fields['phone_number'].required = True
         self.fields['email'].required = True
+        self.fields['address'].required = True
