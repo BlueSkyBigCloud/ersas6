13aacb0ea745105da4508e0b4c7849368382d93b
BlueSkyBigCloud <russeyl91@gmail.com>
2026-08-17 10:22:29 -0500

    PROFOROPS stripe test

--- business/views.py
+++ business/views.py
@@ -327,6 +327,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def stripe_webhook(request):
+    print(">>> VIEW TWO IS ACTIVE")
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
