e23f1de65df3dbf53ad1631b645f262c4d17aeab
BlueSkyBigCloud <russeyl91@gmail.com>
2026-08-19 21:11:37 -0500

    PROFOROPS dataintegration 8

--- dataintegration/admin.py
+++ dataintegration/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    DataImport,
+    DataImportColumn,
+)
+
+
+@admin.register(DataImport)
+class DataImportAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "company",
+        "created_by",
+        "original_filename",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "status",
+        "company",
+        "created_at",
+    )
+
+    search_fields = (
+        "id",
+        "original_filename",
+        "created_by__email",
+        "company__name",
+    )
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+    list_per_page = 50
+
+
+@admin.register(DataImportColumn)
+class DataImportColumnAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "data_import",
+        "source_column",
+        "target_field",
+        "column_order",
+        "is_required",
+        "is_mapped",
+    )
+
+    list_filter = (
+        "is_required",
+        "is_mapped",
+    )
+
+    search_fields = (
+        "source_column",
+        "target_field",
+        "data_import__original_filename",
+    )
+
+    ordering = (
+        "data_import",
+        "column_order",
+    )
+
+    list_per_page = 100
