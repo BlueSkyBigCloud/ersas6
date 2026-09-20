079efc75b5df3118bf11c43aff7eb1bb164762a9
BlueSkyBigCloud <russeyl91@gmail.com>
2026-09-19 21:47:34 -0500

    PROFOROPS REPORTS 23

--- reports/urls.py
+++ reports/urls.py
@@ -8,7 +8,7 @@ app_name = "reports"
 
 urlpatterns = [
     path("reports_dashboard", views.reports_dashboard, name="reports_dashboard"),
-    path( "", views.reports_dashboard, name="reports_dashboard", ),
+    path( "", views.reports_interactive, name="reports_dashboard", ),
     path( "service-requests/", views.service_request_report, name="service_request_report", ), 
     path( "invoices/", views.invoice_report, name="invoice_report", ), 
     path( "employees/", views.employee_report, name="employee_report", ),
